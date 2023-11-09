486d32bf7825e18f5589d650e36bec9fd5a89d41
techsideofficial <wienerwolf77@gmail.com>
2023-11-10 10:59:23 +1300

    Build #3 - First Release

---
+++ assets/js/5e95c892.a024c701.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjoysteem_new=self.webpackChunkjoysteem_new||[]).push([[661],{1892:(e,s,r)=>{r.r(s),r.d(s,{default:()=>o});r(7294);var n=r(6010),t=r(1944),u=r(5281),a=r(8790),c=r(7961),l=r(5893);function o(e){return(0,l.jsx)(t.FG,{className:(0,n.Z)(u.k.wrapper.docsPages),children:(0,l.jsx)(c.Z,{children:(0,a.H)(e.route.routes)})})}}}]);
--- site-new/joysteem-new/docusaurus.config.js
+++ site-new/joysteem-new/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://joysteem.web.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/site-new/joysteem-new/build/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
