4dcadcc4235c3c1a36a93a1c250c4c99475af389
techsideofficial <wienerwolf77@gmail.com>
2023-11-10 10:43:55 +1300

    Build #2

---
+++ site-new/joysteem-new/docs/Tools/Arduino NeoPixel.md
@@ -0,0 +1,9 @@
+# Arduino NeoPixel Generator
+
+This tool is used to generate code for an Arduino board with a NeoPixel Strip connected to it.
+
+:::danger
+This is only for NeoPixel Strips. NeoPixel Rings WILL NOT WORK!
+:::
+
+You can try it out [here](/beta/neopixel/index.html).
--- site-new/joysteem-new/docusaurus.config.js
+++ site-new/joysteem-new/docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'JoySteem',
+  tagline: 'I hope you like the new site!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'techsideofficial', // Usually your GitHub org/user name.
+  projectName: 'JoySteem', // Usually your repo name.
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
@@ -76,7 +76,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'mainSidebar',
             position: 'left',
-            label: 'test Title',
+            label: 'JoySteem',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
