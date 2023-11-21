4fcdcde73ea97968b62c6f7cc11342953a02e598
techsideofficial <wienerwolf77@gmail.com>
2023-11-21 14:46:26 +1300

    upload2

--- percentages/webpage/progressbar.js
+++ percentages/webpage/progressbar.js
@@ -1,2 +1,9 @@
-fetch('https://us-central1-joysteem.cloudfunctions.net/recworks-progress-mean')
-  .then(response => customElements.define('prog-bar', response))
+const { Console } = require("console");
+
+var meanNumber = fetch('https://us-central1-joysteem.cloudfunctions.net/recworks-progress-mean').response
+var progBar = fetch('https://us-central1-joysteem.cloudfunctions.net/progress/' + meanNumber).response
+
+var img = document.createElement("img");
+img.src = progBar;
+var src = document.getElementById("header");
+src.appendChild(img);
